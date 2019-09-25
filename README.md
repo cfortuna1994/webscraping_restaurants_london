50f3cf0df31ff47c3b68259f6c51e7499b758351
Davidson <davidson.nascimento@goodeggs.com>
2019-09-24 21:34:28 -0300

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+
+```sh
+# install dependencies
+pip install -r requirements.txt
+
+# save dependencies
+pip freeze > requirements.txt
+```
--- get_pub.py
+++ get_pub.py
@@ -4,11 +4,12 @@ import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
-import statistics 
+import logging
+# import statistics
 import re
 
 
-#url = 'https://www.tripadvisor.ie/Restaurant_Review-g186338-d6635485-Reviews-The_Clink_Restaurant-London_England.html'
+# url = 'https://www.tripadvisor.ie/Restaurant_Review-g186338-d6635485-Reviews-The_Clink_Restaurant-London_England.html'
 
 url = 'https://www.tripadvisor.ie/Restaurants-g186338-London_England.html'
 driver = webdriver.Chrome()
@@ -20,149 +21,158 @@ email = []
 phone = []
 website = []
 
+
 def get_website(url):
 
-	driver = url
-	html_source = driver.page_source
-	try:
-		website = re.findall(r'website":"(.*?)","email', html_source)
-		website = website
-	except:
-		website = 'ERROR'
+    driver = url
+    html_source = driver.page_source
+    try:
+        website = re.findall(r'website":"(.*?)","email', html_source)
+        website = website
+    except:
+        website = 'ERROR'
 
+    print(website)
 
-	print(website)
-	
-	return website
+    return website
+
 
 def get_phone(url):
 
-	driver = url
+    driver = url
 
-	try:
-		phone = driver.find_element_by_xpath('//span[@class="detail  is-hidden-mobile"]')
-		phone = phone.text
-	except:
-		phone = 'ERROR'
+    try:
+        phone = driver.find_element_by_xpath(
+            '//span[@class="detail  is-hidden-mobile"]')
+        phone = phone.text
+    except:
+        phone = 'ERROR'
 
-	print(phone)
-	return phone
+    print(phone)
+    return phone
+
 
 def get_address(url):
 
-	driver = url
-	try: 
-		street = driver.find_element_by_xpath('//span[@class="street-address"]')
-		street = street.text
-		locality = driver.find_element_by_xpath('//span[@class="locality"]')
-		locality = locality.text
+    driver = url
+    try:
+        street = driver.find_element_by_xpath(
+            '//span[@class="street-address"]')
+        street = street.text
+        locality = driver.find_element_by_xpath('//span[@class="locality"]')
+        locality = locality.text
 
-	except: 
-		street = 'ERROR'
-		locality = 'ERROR'     
+    except:
+        street = 'ERROR'
+        locality = 'ERROR'
 
-	print(street)
-	print(locality)
+    print(street)
+    print(locality)
 
-	address = street +', ' + locality
-	
+    address = street + ', ' + locality
 
-	return address
+    return address
 
 
 def get_email(url):
 
-	driver = url
-	html_source = driver.page_source
-	#email = re.findall(r'\w+@\w+', html_source)
-	try:
-		email = re.findall(r'mailto:([^"]*@.[^"]*)', html_source)
-		email = email
-	except:
-		email = 'ERROR'
+    driver = url
+    html_source = driver.page_source
+    # email = re.findall(r'\w+@\w+', html_source)
+    try:
+        email = re.findall(r'mailto:([^"]*@.[^"]*)', html_source)
+        email = email
+    except:
+        email = 'ERROR'
 
-	print(email)
+    print(email)
 
-	return email
+    return email
 
-def save(name,address,email,phone,website):
 
-	df = pd.DataFrame(
-	{'Name': name,
-     'Address' : address,
-     'Email' : email,
-     'Phone': phone,
-     'Website' : website   
-     
-     
-  	})
-	writer = pd.ExcelWriter('restaurants_london.xlsx', engine='xlsxwriter')
+def save(name, address, email, phone, website):
 
-	# Convert the dataframe to an XlsxWriter Excel object.
-	df.to_excel(writer, sheet_name='Sheet1')
+    print(name, address, email, phone)
 
-	# Close the Pandas Excel writer and output the Excel file.
-	writer.save()
+    # df = pd.DataFrame(
+    #     {'Name': name,
+    #      'Address': address,
+    #      'Email': email,
+    #      'Phone': phone,
+    #      'Website': website
+
+
+    #      })
+    # writer = pd.ExcelWriter('restaurants_london.xlsx', engine='xlsxwriter')
+
+    # # Convert the dataframe to an XlsxWriter Excel object.
+    # df.to_excel(writer, sheet_name='Sheet1')
+
+    # # Close the Pandas Excel writer and output the Excel file.
+    # writer.save()
+
 
 def get_info(driver):
-	time.sleep(4)
-	restaurants = ui.WebDriverWait(driver, 15).until(lambda driver: driver.find_elements_by_xpath('//a[@class="property_title"]'))
+    print('get_info')
+    time.sleep(4)
+    restaurants = ui.WebDriverWait(driver, 15).until(
+        lambda driver: driver.find_elements_by_xpath('//a[@class="property_title"]'))
 
-	for res in restaurants:
-		try:
-			name.append(res.text)
-			print(res.text)
-			res.click()
-			driver.switch_to.window(driver.window_handles[1])
-			print('\n')	
-			try:
-				address.append(get_address(driver))
-				email.append(get_email(driver))
-				phone.append(get_phone(driver))
-				website.append(get_website(driver))
-				driver.close()
-			except:
-				print('ERROR')
+    print(restaurants)
 
-			driver.switch_to.window(driver.window_handles[0])
-		except:
-			save(name,address,email,phone,website)
-			
-	#Next = driver.find_element_by_xpath('//a[@class="nav next rndBtn ui_button primary taLnk"]')
-	#Next.click()
-	previous = driver.find_element_by_xpath('//a[@class="nav previous rndBtn ui_button primary taLnk"]') 
-	previous.click()	
-	print('\n\n****NEXT**** ')
-	save(name,address,email,phone,website)
+    for res in restaurants:
+        try:
+            name.append(res.text)
+            print(res.text)
+            res.click()
+            driver.switch_to.window(driver.window_handles[1])
+            print('\n')
+            try:
+                address.append(get_address(driver))
+                email.append(get_email(driver))
+                phone.append(get_phone(driver))
+                website.append(get_website(driver))
+                driver.close()
+            except:
+                print('ERROR')
 
-def next(url,contador,decrementador):
-	driver = url
-	try:
-		Next = ui.WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_xpath('//a[@class="nav next rndBtn ui_button primary taLnk"]'))
-		time.sleep(1)
-		Next.click()
-		decrementador = decrementador + 1
-	except:
-		print('ta na except, meu decrementador é:' + str(decrementador))
-		time.sleep(4)
-	return decrementador
+            driver.switch_to.window(driver.window_handles[0])
+        except:
+            save(name, address, email, phone, website)
+
+    # Next = driver.find_element_by_xpath('//a[@class="nav next rndBtn ui_button primary taLnk"]')
+    # Next.click()
+    previous = driver.find_element_by_xpath(
+        '//a[@class="nav previous rndBtn ui_button primary taLnk"]')
+    previous.click()
+    print('\n\n****NEXT**** ')
+    save(name, address, email, phone, website)
+
+
+def next(url, contador, decrementador):
+    driver = url
+    try:
+        Next = ui.WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_xpath(
+            '//a[@class="nav next rndBtn ui_button primary taLnk"]'))
+        time.sleep(1)
+        Next.click()
+        decrementador = decrementador + 1
+    except:
+        print('ta na except, meu decrementador e:' + str(decrementador))
+        time.sleep(4)
+    return decrementador
+
 
 cont = 0
 
 
-
-
-
 p640 = driver.find_elements_by_xpath('//a[@class="pageNum taLnk"]')
 for x in p640:
-	cont += 1
-	#print('p640')
-	if(cont == 6):
-		print(x.text)
-		x.click()
-	
-
-
+    cont += 1
+    # print('p640')
+    if(cont == 6):
+        print(x.text)
+        x.click()
 
 
 contador = 245
@@ -171,21 +181,24 @@ decrementador = 0
 while decrementador <= contador:
 	decrementador = next(driver,contador,decrementador)
 	print('contador:' + str(contador) + ', decrementador' + str(decrementador))
-'''		
+'''
 while True:
-	try:
-		print('while')
-		get_info(driver)
-	except:
-		save(name,address,email,phone,website)
+    try:
+        print('while')
+        get_info(driver)
+    except Exception as e:
+        logging.exception("message")
+
+        save(name, address, email, phone, website)
 
 '''
 
-    #qty_followup = fields.Text(string = 'Follow up', compute = '_delivery_followup')
-    
+    # qty_followup = fields.Text(string = 'Follow up', compute = '_delivery_followup')
+
     @api.depends('product_uom_qty', 'qty_delivered')
     def _delivery_followup(self):
-        
+
         qty_followup = self.product_uom_qty - self.qty_delivered
 
         return qty_followup
+'''
